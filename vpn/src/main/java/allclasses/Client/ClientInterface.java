c6e7bb4933cc78230560b5b6166e7dd106dafc8c
Yatharth Gupta <34278681+yath2307@users.noreply.github.com>
2020-04-25 20:17:28 +0530

    Add files via upload

---
+++ vpn/src/main/java/allclasses/Client/ClientInterface.java
@@ -0,0 +1,10 @@
+package allclasses.Client;
+
+public interface ClientInterface {
+
+	//boolean return type signifies if call succeeded in performing expected work.
+	public boolean startClient( String host, int port, String sharedKey ); //Start client and do handshake. Expected call GUIInterface.connectionReady()
+	public void endConnection(); //End connection
+	public boolean sendMessage( String s ); //sends a message
+	public void receiveMessages(); //infinite blocking loop for receiving messages
+}
